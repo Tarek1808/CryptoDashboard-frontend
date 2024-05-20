29aee056488ab40004e38329edf2d7def54635b1
Florian <florian.serrault@hotmail.fr>
2024-05-20 17:06:27 +0200

    menubar

--- components/AddWallet.js
+++ components/AddWallet.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import styles from '../styles/AddWallet.module.css';
-<<<<<<< HEAD
 import Menubar from './Menubar';
 import AddBlockchain from './AddBlockchain';
-=======
 import Header from './Header';
 
->>>>>>> 851cf625ae4b53e2f779706e7abe1b65008c7e6d
 
 function AddWallet() {
     return (
--- pages/addWallet.js
+++ pages/addWallet.js
@@ -1,17 +1,12 @@
 import AddWallet from '../components/AddWallet';
 
 
-<<<<<<< HEAD
-function AddWalletPage() {
-  return <AddWallet />;
-=======
 
 
 function AddWalletPage() {
   return (<div>
     <AddWallet />
     </div>);
->>>>>>> 851cf625ae4b53e2f779706e7abe1b65008c7e6d
   
 }
 
