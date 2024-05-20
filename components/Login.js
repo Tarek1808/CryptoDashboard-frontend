590eb6985ba9b6a4e3f61ce62528f08e01660081
Alxvke <leefccn@hotmail.com>
2024-05-20 11:15:36 +0200

    front v1.1 login

--- components/Login.js
+++ components/Login.js
@@ -1,17 +1,15 @@
 import styles from '../styles/Login.module.css';
-//import SignUp from './SignUp';
-//import SignIn from './SignIn';
 
 function Login() {
     return (
         <div className={styles.container}>
             <header className={styles.header}>Dashboard Crypto</header>
             <main className={styles.main}>
-                <div className={styles.presentation}>
-                   
-                    <h2>Welcome to Dashboard Crypto!</h2>
-                    <p>Here you can manage your crypto assets...
-                    Le Lorem Ipsum est simplement du faux texte employé dans la composition et la
+                <div className={styles.content}>
+                    <div className={styles.presentation}>
+                        <h2>Welcome to Dashboard Crypto!</h2>
+                        <p>Here you can manage your crypto assets...
+                        Le Lorem Ipsum est simplement du faux texte employé dans la composition et la
 mise en page avant impression. Le Lorem Ipsum est le faux texte standard de
 l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla
 ensemble des morceaux de texte pour réaliser un livre spécimen de polices de
@@ -30,14 +28,19 @@ bureautique informatique, sans que son contenu n'en soit modifié. Il a été
 popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant
 des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des
 applications de mise en page de texte, comme Aldus PageMaker.
-                    </p>
-                    
+                        </p>
+                        
+                    </div>
+                    <div className={styles.auth}>
+                        
+                        <button onClick={() => console.log("Sign In")}>Sign In</button>
+                        <button onClick={() => console.log("Sign Up")}>Sign Up</button>
+                    </div>
                 </div>
-                <div className={styles.auth}>
-                   
-                    <button onClick={() => console.log("Sign In")}>Sign In</button>
-                    <button onClick={() => console.log("Sign Up")}>Sign Up</button>
-                </div>
+                
+                <p><a href="http://localhost:3001/">Login</a></p>
+                <p><a href="http://localhost:3001/addWallet">AddWallet</a></p>
+                <p><a href="http://localhost:3001/wallets">Wallets</a></p>
             </main>
         </div>
     );
