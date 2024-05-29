90b64eaabd8721fbe296c3e58360775565e829b8
Tarek1808 <164371513+Tarek1808@users.noreply.github.com>
2024-05-29 17:46:56 +0200

    taff de flo

--- components/SignUp.js
+++ components/SignUp.js
@@ -8,7 +8,9 @@ const BACKEND_ADDRESS = "https://crypto-dashboard-backend-gamma.vercel.app"
 
 
 const EMAIL_REGEX =
-  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+const PASSWORD_REGEX = /(?=^.{6,}$)(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])(?=.*[^A-Za-z0-9]).*/;
 
 function SignUp() {
     const dispatch = useDispatch()
@@ -17,12 +19,23 @@ function SignUp() {
     const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    
- 
+    const [errorPassword, setErrorPassword] = useState(false);
+    const [errorMail, setErrorMail] = useState(false)
+
 
 
     const handleSubmit = () => {
-        if (EMAIL_REGEX.test(email)){
+        //on verifie la conception de l'adresse email
+        if (!EMAIL_REGEX.test(email)) {
+            setErrorMail(true);
+            return;
+        };
+        //on verifie le mot de passe
+        if (!PASSWORD_REGEX.test(password)) {
+            setErrorPassword(true);
+            return;
+        };
+
         fetch(`${BACKEND_ADDRESS}/users/signup`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
@@ -36,32 +49,33 @@ function SignUp() {
                     setEmail('')
                     setPassword('')
                     setUsername('')
-                    
+
                     fetch(`${BACKEND_ADDRESS}/cryptos/price`, {
                         method: 'POST',
                         headers: { 'Content-Type': 'application/json' },
                     }).then(response => response.json())
-                    .then(data => {
-                        if (data.result) {
-                            console.log("prices updated")
-                        }
-                    })
+                        .then(data => {
+                            if (data.result) {
+                                console.log("prices updated")
+                            }
+                        })
                     router.push('/addWallet')
                 } else {
                     console.log("erreur sign up")
                 }
             });
-            
-                // res.json({ result:false, error: 'Invalid email adress'});
-                // return;
-              }
-              else console.log('error total') 
-              
-    };
+
+        // res.json({ result:false, error: 'Invalid email adress'});
+        // return;
+    }
+
+
+
 
     return (
         <div className={styles.container}>
             <p className={styles.title}>Create your CryptoDashboard account</p>
+
             <div className={styles.inputContainer}>
                 <label className={styles.labelCaption}>Username</label>
                 <input type="text" className={styles.input} onChange={(e) => setUsername(e.target.value)} value={username} />
@@ -70,10 +84,21 @@ function SignUp() {
                 <label className={styles.labelCaption}>Email</label>
                 <input type="text" className={styles.input} onChange={(e) => setEmail(e.target.value)} value={email} />
             </div>
+            {errorMail && (
+
+                <p className={styles.error}>Invalid email address</p>
+
+            )}
             <div className={styles.inputContainer}>
                 <label className={styles.labelCaption}> Password </label>
                 <input type="password" className={styles.input} onChange={(e) => setPassword(e.target.value)} value={password} />
             </div>
+            {errorPassword && (
+                <div>
+                    <p className={styles.error}>Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character, and be at least 6 characters long.</p>
+                </div>
+            )}
+
             <button className={styles.button} onClick={() => handleSubmit()}>SIGN UP</button>
         </div>
     );
