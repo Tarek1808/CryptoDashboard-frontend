4ce049aaf14e24284c1e6c2fa511de7b16d0a2f4
Tarek1808 <164371513+Tarek1808@users.noreply.github.com>
2024-05-30 09:57:01 +0200

    news en plus

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['i-invdn-com.investing.com', 'i.kinja-img.com' ,'images.bfmtv.com', 'www.broker-forex.fr','cdn.zonebourse.com'],
+    domains: ['i-invdn-com.investing.com', 'i.kinja-img.com' ,'images.bfmtv.com', 'www.broker-forex.fr','cdn.zonebourse.com', 'media.lesechos.com'],
   },
 };
 
