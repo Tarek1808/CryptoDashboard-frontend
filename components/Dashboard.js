e1f8d9a0e94fe41ef8f708115992d2f56042dfd5
clement <clement.laumond@gmail.com>
2024-05-30 16:42:02 +0200

    dashboard css

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -1,42 +1,61 @@
-
 export default Dashboard;
-import styles from '../styles/Wallets.module.css';
-import MenuBar from './MenuBar';
-import Header from './Header';
-import React, { useEffect, useState } from 'react';
-import TableDash from './TableDash';
-import { useSelector } from 'react-redux';
+import styles from "../styles/Dashboard.module.css";
+import MenuBar from "./MenuBar";
+import Header from "./Header";
+import React, { useEffect, useState } from "react";
+import TableDash from "./TableDash";
+import { useSelector } from "react-redux";
 
 function Dashboard() {
   const wallets = useSelector((state) => state.wallets.value);
-  console.log("wallets", wallets)
+  console.log("wallets", wallets);
 
-  const value = useSelector((state) => state.value.value)
-  const totalValueThisConnection = value.totalValue
-  console.log("value", value)
+  const value = useSelector((state) => state.value.value);
+  const totalValueThisConnection = value.totalValue;
+  console.log("value", value);
 
-  const user = useSelector((state) => state.user.value)
+  const user = useSelector((state) => state.user.value);
 
-  const [valueLastConnection, setValueLastConnection] = useState(0)
+  const [valueLastConnection, setValueLastConnection] = useState(0);
 
-  const [formattedDate, setFormattedDate] = useState('')
+  const [formattedDate, setFormattedDate] = useState("");
 
   useEffect(() => {
     if (user.totalValue && user.totalValue.length > 1) {
       const lastValue = user.totalValue[user.totalValue.length - 2]?.value;
       setValueLastConnection(lastValue);
 
-      const dateLastConnection = user.totalValue[user.totalValue.length - 2]?.date;
+      const dateLastConnection =
+        user.totalValue[user.totalValue.length - 2]?.date;
       const date = new Date(dateLastConnection);
-      setFormattedDate(date.toLocaleDateString('fr-FR', {
-        day: '2-digit',
-        month: '2-digit',
-        year: 'numeric',
-      }));
+      setFormattedDate(
+        date.toLocaleDateString("fr-FR", {
+          day: "2-digit",
+          month: "2-digit",
+          year: "numeric",
+        })
+      );
     }
   }, [user.totalValue]);
 
-  const percentageChange = valueLastConnection ? ((totalValueThisConnection - valueLastConnection) / totalValueThisConnection) * 100 : 0;
+  const percentageChange = valueLastConnection
+    ? ((totalValueThisConnection - valueLastConnection) /
+        totalValueThisConnection) *
+      100
+    : 0;
+
+  let stylePercentage = {};
+  let plusOrMinus = "";
+  if (percentageChange > 0) {
+    stylePercentage = { color: "green" };
+    plusOrMinus = "+";
+  } else if (percentageChange < 0) {
+    stylePercentage = { color: "red" };
+    plusOrMinus = "";
+  } else {
+    stylePercentage = { color: "white" };
+    plusOrMinus = "";
+  }
 
   return (
     <div>
@@ -48,13 +67,23 @@ function Dashboard() {
           <MenuBar />
         </div>
         <div className={styles.rightContent}>
-          <h1 className={styles.title}>
-            Dashboard
-          </h1>
+          <h1 className={styles.title}>Dashboard</h1>
           <div className={styles.table}>
             <div className={styles.summary}>
-              <h2>{totalValueThisConnection.toFixed(2)} $</h2>
-              {formattedDate && <p>{percentageChange.toFixed(2)}% since your last connection ({formattedDate})</p>}
+              <h2 style={stylePercentage} className={styles.totalValue}>
+                {totalValueThisConnection.toFixed(2)} $
+              </h2>
+              {formattedDate && (
+                <p className={styles.lastConnection}>
+                  <span style={stylePercentage}>
+                    {plusOrMinus}
+                    {percentageChange.toFixed(2)}%
+                  </span>{" "}
+                  <span className={styles.sinceLastConnection}>
+                    since your last connection ({formattedDate})
+                  </span>
+                </p>
+              )}
             </div>
             <TableDash />
           </div>
--- components/Reporting.js
+++ components/Reporting.js
@@ -1,61 +1,61 @@
-import styles from '../styles/Reporting.module.css';
-import MenuBar from './MenuBar';
-import Header from './Header';
-import { Line, Pie } from 'react-chartjs-2'
-import { useSelector } from 'react-redux';
-import 'chart.js/auto';
+import styles from "../styles/Reporting.module.css";
+import MenuBar from "./MenuBar";
+import Header from "./Header";
+import { Line, Pie } from "react-chartjs-2";
+import { useSelector } from "react-redux";
+import "chart.js/auto";
 
 function Reporting() {
+  const user = useSelector((state) => state.user.value);
+  const tableTotalValue = user.totalValue;
 
-  const user = useSelector((state) => state.user.value)
-  const tableTotalValue = user.totalValue
+  const value = useSelector((state) => state.value.value);
+  const totalValue = value.totalValue;
+  const cryptoData = value.cryptoData;
 
-  const value = useSelector((state) => state.value.value)
-  const totalValue = value.totalValue
-  const cryptoData = value.cryptoData
-
-  const pieLabels = []
-  const pieDatasets = []
+  const pieLabels = [];
+  const pieDatasets = [];
   cryptoData?.forEach((crypto) => {
     if (crypto.value > 0) {
-      pieLabels.push(crypto.name)
-      pieDatasets.push(crypto.value / totalValue)
+      pieLabels.push(crypto.name);
+      pieDatasets.push(crypto.value / totalValue);
     }
-  })
+  });
 
   const pieData = {
     labels: pieLabels,
     datasets: [
       {
         data: pieDatasets,
-        backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
-        hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
+        backgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
+        hoverBackgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
       },
     ],
   };
 
-  const lineLabels = []
-  const lineDataset = []
-  tableTotalValue?.forEach(totalValue => {
-    const datePreviousConnections = new Date(totalValue.date)
-    const formattedDatePreviousConnections = datePreviousConnections.toLocaleDateString('fr-FR', {
-      day: '2-digit',
-      month: '2-digit',
-      year: 'numeric',
-    });
-    lineLabels.push(formattedDatePreviousConnections)
-    lineDataset.push(totalValue.value)
-  })
+  const lineLabels = [];
+  const lineDataset = [];
+  tableTotalValue?.forEach((totalValue) => {
+    const datePreviousConnections = new Date(totalValue.date);
+    const formattedDatePreviousConnections =
+      datePreviousConnections.toLocaleDateString("fr-FR", {
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric",
+      });
+    lineLabels.push(formattedDatePreviousConnections);
+    lineDataset.push(totalValue.value);
+  });
 
   const lineData = {
     labels: lineLabels,
     datasets: [
       {
-        label: 'Evolution of your portfolio',
+        label: "Evolution of your portfolio",
         data: lineDataset,
         fill: false,
-        backgroundColor: 'rgb(75, 192, 192)',
-        borderColor: 'rgba(75, 192, 192, 0.2)',
+        backgroundColor: "rgb(75, 192, 192)",
+        borderColor: "rgba(75, 192, 192, 0.2)",
       },
     ],
   };
@@ -70,12 +70,10 @@ function Reporting() {
           <MenuBar />
         </div>
         <div className={styles.rightContent}>
-          <h1 className={styles.title}>
-            Reporting
-          </h1>
+          <h1 className={styles.title}>Reporting</h1>
           <div className={styles.charts}>
-            < Pie className={styles.pie} data={pieData} />
-            < Line className={styles.line} data={lineData} />
+            <Pie className={styles.pie} data={pieData} />
+            <Line className={styles.line} data={lineData} />
           </div>
         </div>
       </div>
